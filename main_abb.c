e2a6a5ece2aafd72fc5aa6458c5964fe24f58281
Martin <maartin.212@gmail.com>
2018-11-11 02:14:55 -0300

    initial commit

---
+++ main_abb.c
@@ -0,0 +1,18 @@
+#include "abb.h"
+#include "testing.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void pruebas_abb_alumno(void);
+
+/* ******************************************************************
+ *                        PROGRAMA PRINCIPAL
+ * *****************************************************************/
+
+int main(int argc, char *argv[])
+{
+    printf("~~~ PRUEBAS ALUMNO ~~~\n");
+    pruebas_abb_alumno();
+
+    return failure_count() > 0;
+}
